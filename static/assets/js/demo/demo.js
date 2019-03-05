5936e0a384ecd386272d22a5a054f439ceda1f3b
daniel <danielmontecinoszambra@gmail.com>
2019-03-05 15:34:19 -0300

    Base para trabajar login y index

--- static/assets/js/demo/demo.js
+++ static/assets/js/demo/demo.js
@@ -17,8 +17,8 @@ var Demo = function() {
         return;
       }
       e.preventDefault;
-      alert('Your form has submitted!');
-      return false;
+//          alert('Your hola has submitted!');
+//          return false;
     });
 
     // give file-upload preview onclick functionality
